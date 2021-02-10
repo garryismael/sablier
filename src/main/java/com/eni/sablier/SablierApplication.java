9152e4f677b543153be45693df285177c6c32af5
garry <tahinjanaharygarry@gmail.com>
2021-02-10 15:51:03 +0300

    project files

---
+++ src/main/java/com/eni/sablier/SablierApplication.java
@@ -0,0 +1,13 @@
+package com.eni.sablier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SablierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SablierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eni/sablier/SablierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eni.sablier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SablierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
